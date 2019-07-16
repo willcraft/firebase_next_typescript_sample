784c0b8bd768d5e58e02458ea5de6397afbabc6e
Yoshitaka Sasaki <y_sasaki+gitkraken@willcraft.jp>
2019-07-16 18:58:06 +0900

    WIP: express化

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
+const express = require('express');
 const next = require('next');
 
+// const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev, conf: { distDir: './dist' }})
 const handle = nextApp.getRequestHandler()
@@ -8,8 +10,24 @@ const handle = nextApp.getRequestHandler()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+
 exports.app = functions.https.onRequest((req, res) => {
-  console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-  // res.send("Hello from Firebase!!!!");
-  return nextApp.prepare().then(() => handle(req, res))
+
+  return nextApp.prepare().then(() => {
+    const server = express()
+
+    server.get('/posts/:id', (req, res) => {
+      return nextApp.render(req, res, '/posts', { id: req.params.id })
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    // server.listen(port, err => {
+    //   if (err) throw err
+    //   console.log(`> Ready on http://localhost:${port}`)
+    // })
+  });
 });
